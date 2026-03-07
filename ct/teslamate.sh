5b54dedb456ecb4ecdf1bae6dafc740479c00f2a
Aaron Vaz <vazaaron08@gmail.com>
2026-03-07 15:35:19 +0000

    test(teslamate): also source build.func from dev branch

--- ct/teslamate.sh
+++ ct/teslamate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/aaron-vaz/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/aaron-vaz/ProxmoxVE/dev/contribution/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: aaron-vaz
 # License: MIT | https://github.com/aaron-vaz/ProxmoxVE/raw/main/LICENSE
